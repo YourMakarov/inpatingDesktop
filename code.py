52cebbab610be1bc075b6cb03445c1df07b81f82
Gleb_Makarov <74141032+YourMakarov@users.noreply.github.com>
2022-07-05 18:16:23 +0300

    Update code.py

--- code.py
+++ code.py
@@ -30,18 +30,17 @@ class Window(QWidget):
 
         #create media player object
         self.mediaPlayer1 = QMediaPlayer(None, QMediaPlayer.VideoSurface)
-        self.mediaPlayer2 = QMediaPlayer(None, QMediaPlayer.VideoSurface)
+
 
         #create videowidget object
 
         videowidget1 = QVideoWidget()
-        videowidget2 = QVideoWidget()
+
 
         #create open button
         openBtn1 = QPushButton('Open Video')
         openBtn1.clicked.connect(lambda: self.open_file(self.mediaPlayer1, self.playBtn1))
-        #openBtn2 = QPushButton('Open Video')
-        #openBtn2.clicked.connect(lambda: self.open_file(self.mediaPlayer2, self.playBtn2))
+
 
 
         #create button for playing
@@ -49,10 +48,7 @@ class Window(QWidget):
         self.playBtn1.setEnabled(False)
         self.playBtn1.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
         self.playBtn1.clicked.connect(lambda: self.play_video(self.mediaPlayer1))
-        self.playBtn2 = QPushButton()
-        self.playBtn2.setEnabled(False)
-        self.playBtn2.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
-        self.playBtn2.clicked.connect(lambda: self.play_video(self.mediaPlayer2))
+
 
 
 
@@ -60,61 +56,46 @@ class Window(QWidget):
         self.slider1 = QSlider(Qt.Horizontal)
         self.slider1.setRange(0, 0)
         self.slider1.sliderMoved.connect(self.set_position1)
-        self.slider2 = QSlider(Qt.Horizontal)
-        self.slider2.setRange(0, 0)
-        self.slider2.sliderMoved.connect(self.set_position2)
 
 
+        self.is_video_transformed = False
 
         #create label
         self.label1 = QLabel()
         self.label1.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Maximum)
-        self.label2 = QLabel()
-        self.label2.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Maximum)
+
         self.filename = ''
 
         #create hbox layout
-        hboxLayout1 = QHBoxLayout()
-        hboxLayout1.setContentsMargins(0, 0, 0, 0)
-        hboxLayout2 = QHBoxLayout()
-        hboxLayout2.setContentsMargins(0, 0, 0, 0)
+        self.hboxLayout1 = QHBoxLayout()
+        self.hboxLayout1.setContentsMargins(0, 0, 0, 0)
 
         #set widgets to the hbox layout
-        hboxLayout1.addWidget(openBtn1)
-        hboxLayout1.addWidget(self.playBtn1)
-        hboxLayout1.addWidget(self.slider1)
-        #hboxLayout2.addWidget(openBtn2)
-        hboxLayout2.addWidget(self.playBtn2)
-        hboxLayout2.addWidget(self.slider2)
+        self.hboxLayout1.addWidget(openBtn1)
+        self.hboxLayout1.addWidget(self.playBtn1)
+        self.hboxLayout1.addWidget(self.slider1)
 
 
 
         #create vbox layout
-        vboxLayout1 = QVBoxLayout()
-        vboxLayout1.addWidget(videowidget1)
-        vboxLayout1.addLayout(hboxLayout1)
-        vboxLayout1.addWidget(self.label1)
-        vboxLayout2 = QVBoxLayout()
-        vboxLayout2.addWidget(videowidget2)
-        vboxLayout2.addLayout(hboxLayout2)
-        vboxLayout2.addWidget(self.label2)
+        self.vboxLayout1 = QVBoxLayout()
+        self.vboxLayout1.addWidget(videowidget1)
+        self.vboxLayout1.addLayout(self.hboxLayout1)
+        self.vboxLayout1.addWidget(self.label1)
 
-        newLayout = QVBoxLayout()
-        newLayout.addLayout(vboxLayout1)
+        self.newLayout = QVBoxLayout()
+        self.newLayout.addLayout(self.vboxLayout1)
 
         self.bigBtn = QPushButton()
         self.bigBtn.setText('TO DO SmThing with videofile')
         self.bigBtn.setEnabled(False)
         self.bigBtn.clicked.connect(self.do_smthing_with_videofile)
 
-        newLayout.addWidget(self.bigBtn)
+        self.newLayout.addWidget(self.bigBtn)
 
-        newLayout.addLayout(vboxLayout2)
-
-        self.setLayout(newLayout)
+        self.setLayout(self.newLayout)
 
         self.mediaPlayer1.setVideoOutput(videowidget1)
-        self.mediaPlayer2.setVideoOutput(videowidget2)
 
 
         #media player signals
@@ -122,9 +103,7 @@ class Window(QWidget):
         self.mediaPlayer1.stateChanged.connect(lambda: self.mediastate_changed(self.mediaPlayer1, self.playBtn1))
         self.mediaPlayer1.positionChanged.connect(self.position_changed1)
         self.mediaPlayer1.durationChanged.connect(self.duration_changed1)
-        self.mediaPlayer2.stateChanged.connect(lambda: self.mediastate_changed(self.mediaPlayer2, self.playBtn2))
-        self.mediaPlayer2.positionChanged.connect(self.position_changed2)
-        self.mediaPlayer2.durationChanged.connect(self.duration_changed2)
+
 
     def open_file(self, player, btn):
         self.filename, _ = QFileDialog.getOpenFileName(self, "Open Video")
@@ -177,8 +156,37 @@ class Window(QWidget):
         self.label2.setText("Error: " + self.mediaPlayer2.errorString())
 
     def do_smthing_with_videofile(self):
-        self.mediaPlayer2.setMedia(QMediaContent(QUrl.fromLocalFile(self.filename)))
-        self.playBtn2.setEnabled(True)
+        if self.is_video_transformed == False:
+            self.is_video_transformed = True
+            self.mediaPlayer2 = QMediaPlayer(None, QMediaPlayer.VideoSurface)
+            videowidget2 = QVideoWidget()
+            self.playBtn2 = QPushButton()
+            self.playBtn2.setEnabled(False)
+            self.playBtn2.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
+            self.playBtn2.clicked.connect(lambda: self.play_video(self.mediaPlayer2))
+            self.slider2 = QSlider(Qt.Horizontal)
+            self.slider2.setRange(0, 0)
+            self.slider2.sliderMoved.connect(self.set_position2)
+            self.label2 = QLabel()
+            self.label2.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Maximum)
+            self.hboxLayout2 = QHBoxLayout()
+            self.hboxLayout2.setContentsMargins(0, 0, 0, 0)
+            self.hboxLayout2.addWidget(self.playBtn2)
+            self.hboxLayout2.addWidget(self.slider2)
+            self.vboxLayout2 = QVBoxLayout()
+            self.vboxLayout2.addWidget(videowidget2)
+            self.vboxLayout2.addLayout(self.hboxLayout2)
+            self.vboxLayout2.addWidget(self.label2)
+            self.newLayout.addLayout(self.vboxLayout2)
+            self.mediaPlayer2.setVideoOutput(videowidget2)
+            self.mediaPlayer2.stateChanged.connect(lambda: self.mediastate_changed(self.mediaPlayer2, self.playBtn2))
+            self.mediaPlayer2.positionChanged.connect(self.position_changed2)
+            self.mediaPlayer2.durationChanged.connect(self.duration_changed2)
+            self.mediaPlayer2.setMedia(QMediaContent(QUrl.fromLocalFile(self.filename)))
+            self.playBtn2.setEnabled(True)
+        else:
+            self.mediaPlayer2.setMedia(QMediaContent(QUrl.fromLocalFile(self.filename)))
+            self.playBtn2.setEnabled(True)
 
 
 
